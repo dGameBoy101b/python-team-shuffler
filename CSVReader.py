0504731372e919137a953dea70589914bb0cb81c
dGameBoy101b <44672112+dGameBoy101b@users.noreply.github.com>
2022-07-28 18:57:10 +0800

    Created CSV reader tests
    Created unit tests for CSV reader.
    Removed is readable method from CSV reader.
    Added is eof method to CSV reader.
    Added separator class attribute to CSV reader.
    Fixed file not being initialised when constructing CSV reader and opening file.
    Fixed close method of CSV reader not resetting file attribute to none.
    Fixed read line method of CSV reader not returning empty list when no values are found on next line.

--- CSVReader.py
+++ CSVReader.py
@@ -1,9 +1,14 @@
+from io import SEEK_END, SEEK_SET
+from os import remove
+
 class CSVReader():
 	'''A class used to read from CSV files'''
 
+	SEP = ','
+
 	def __init__(self, path:str=None):
+		self.file = None
 		if path is None:
-			self.file = None
 			return
 		if not isinstance(path, str):
 			path = str(path)
@@ -18,6 +23,7 @@ class CSVReader():
 		'''Close the currently open CSV file if one is open'''
 		if self.file is not None:
 			self.file.close()
+		self.file = None
 		return
 
 	def open(self, path:str):
@@ -26,7 +32,7 @@ class CSVReader():
 			path = str(path)
 		if not path.endswith('.csv'):
 			raise ValueError(f'Cannot open a file that is not a csv file: {path}')
-		if self.path is not None:
+		if not self.is_open():
 			self.close()
 		self.file = open(path, 'rt')
 		return
@@ -35,23 +41,58 @@ class CSVReader():
 		'''Does this have a CSV file open'''
 		return self.file is not None
 
-	def is_readable(self)->bool:
-		'''Does this have a CSV file open and is that file not at its end'''
-		return self.is_open() and self.file.readable()
+	def is_eof(self)->bool:
+		'''Is the currently open CSV file at its end'''
+		pos = self.file.tell()
+		self.file.seek(0, SEEK_END)
+		end_pos = self.file.tell()
+		self.file.seek(pos, SEEK_SET)
+		return pos == end_pos
 	
 	def read_line(self)->list[str]:
 		'''Read and return one line of values'''
-		if self.file is None:
+		if not self.is_open():
 			raise RuntimeError('No CSV file open')
-		if not self.file.readable():
-			raise EOFError('CSV file is not readable')
-		return self.file.readline().split(',')
+		if self.is_eof():
+			raise EOFError('The CSV file has no more lines')
+		line = self.file.readline().removesuffix('\n')
+		if CSVReader.SEP not in line:
+			return []
+		values = line.split(CSVReader.SEP)
+		return values
 
 	def read_all(self)->list[list[str]]:
 		'''Read and return all remaining lines of values'''
 		if self.file is None:
 			raise RuntimeError('No CSV file open')
 		lines = list()
-		while self.file.readable():
-			lines.append(self.readline())
+		while not self.is_eof():
+			lines.append(self.read_line())
 		return lines
+
+if __name__ == '__main__':
+	test = CSVReader()
+	assert not test.is_open()
+	assert test.file == None
+	del test
+
+	path = './test.csv'
+	lines = [['abc','123','','def'],[],['qwerty','1234567890'],['jkl','zxcvbnm','']]
+	with open(path, 'wt') as file:
+		file.write(''.join([','.join(line)+'\n' for line in lines]))
+	test = CSVReader(path)
+	assert test.is_open()
+	assert not test.is_eof()
+	assert test.read_line() == lines[0]
+	assert test.is_open()
+	assert not test.is_eof()
+	assert test.read_line() == lines[1]
+	assert test.is_open()
+	assert not test.is_eof()
+	assert test.read_all() == lines[2:]
+	assert test.is_open()
+	assert test.is_eof()
+	test.close()
+	assert not test.is_open()
+	del test
+	remove(path)
